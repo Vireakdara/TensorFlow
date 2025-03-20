2e4fd4ff00fc7d9288e9f1214cbadef1d09b5708
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-20 11:13:13 +0700

    Update tutorial3-neural-network.py

--- tutorial3-neural-network.py
+++ tutorial3-neural-network.py
@@ -1,1 +1,23 @@
+import os
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+physical_devices = tf.config.list_physical_devices('GPU')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Load dataset from mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+
+
+
+
+
+
+
+
+
 
