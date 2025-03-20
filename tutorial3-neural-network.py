52363ba0cb4621bd265c8f956db61510d39ab74b
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-20 22:13:38 +0700

    Update tutorial3-neural-network.py

--- tutorial3-neural-network.py
+++ tutorial3-neural-network.py
@@ -9,6 +9,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Load dataset from mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
+
+# Sequential API (Very convenient, not very flexible)
+model = keras.Sequential(
+    [
+        keras.Input(shape=(28 * 28)),
+        layers.Dense(512, activation="relu"),
+        layers.Dense(256, activation="relu"),
+        layers.Dense(10),
+    ]
+)
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(784)))
+model.add(layers.Dense(512, activation="relu"))
+model.add(layers.Dense(256, activation="relu", name="my_layer"))
+model.add(layers.Dense(10))
 
 
 
