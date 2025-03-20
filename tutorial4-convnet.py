f1e2d122295f90a32f6c892820926056b9e8d76e
V.Dara <76716485+Vireakdara@users.noreply.github.com>
2025-03-20 22:17:54 +0700

    Update tutorial4-convnet.py

--- tutorial4-convnet.py
+++ tutorial4-convnet.py
@@ -11,3 +11,17 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, padding="valid", activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, activation="relu"),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation="relu"),
+        layers.Flatten(),
+        layers.Dense(64, activation="relu"),
+        layers.Dense(10),
+    ]
+)
